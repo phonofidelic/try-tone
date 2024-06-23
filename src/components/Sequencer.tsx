7bf662da5e03a8bcaac93efe079030634b69f292
Christopher Clemons <chris.clemons85@gmail.com>
2024-06-23 13:41:22 +0200

    feat: add initial sequencer functionality

--- src/components/Sequencer.tsx
+++ src/components/Sequencer.tsx
@@ -1,37 +1,138 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import * as Tone from 'tone'
 import { makeGrid } from '../utils'
 import clsx from 'clsx'
+import { ModuleData, useWorkspace } from './Workspace'
+import { DestinationSelect } from './DestinationSelect'
+import { Button } from './Button'
+
+interface Note {
+  id: string
+  note: string
+  isActive: boolean
+}
 
 export function Sequencer() {
-  const [sequence, setSequence] = useState(() => makeGrid())
+  const sequence = useRef(makeGrid())
+  const { nodes } = useWorkspace()
+  const [destinationNode, setDestinationNode] = useState<ModuleData | null>(
+    null,
+  )
+  const [gateNode, setGateNode] = useState<ModuleData | null>(null)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [playingIndex, setPLayingIndex] = useState(0)
+  const [, setActiveNote] = useState<Note | null>(null)
+  const transportRef = useRef(Tone.getTransport())
+  const isRepeatScheduled = useRef(false)
 
-  const handleClick = (noteId: string) => {
-    const updatedSequence = sequence.map((row) => ({
+  const handleClick = (clickedNote: Note) => {
+    sequence.current = sequence.current.map((row) => ({
       ...row,
-      value: row.value.map((note) =>
-        note.id === noteId ? { ...note, isActive: !note.isActive } : note,
-      ),
+      value: row.value.map((note) => {
+        if (note.id === clickedNote.id) {
+          setActiveNote({ ...note, isActive: !note.isActive })
+          return { ...note, isActive: !note.isActive }
+        }
+        return note
+      }),
     }))
-    setSequence(updatedSequence)
   }
 
+  const handleDestinationChange = (id: string) => {
+    setDestinationNode(nodes.find((node) => node.id === id) ?? null)
+  }
+
+  const handleGateChange = (id: string) => {
+    setGateNode(nodes.find((node) => node.id === id) ?? null)
+  }
+
+  const playSequence = () => {
+    if (!transportRef.current) {
+      return
+    }
+
+    transportRef.current.start()
+    setIsPlaying(true)
+  }
+
+  const stopSequence = () => {
+    if (!transportRef.current) {
+      return
+    }
+
+    transportRef.current.stop()
+    setIsPlaying(false)
+  }
+
+  useEffect(() => {
+    if (!transportRef.current || !destinationNode || !gateNode) {
+      return
+    }
+
+    const onPlayNote = (time: Tone.Unit.Time) => {
+      sequence.current.forEach((row) => {
+        const note = row.value[beat]
+        if (destinationNode.type === 'oscillator' && note.isActive) {
+          destinationNode.node.frequency.rampTo(note.note, 0, time)
+        }
+
+        if (gateNode.type === 'envelope' && note.isActive) {
+          gateNode.node.triggerAttackRelease(0.5)
+        }
+      })
+    }
+
+    let beat = 0
+    const onRepeat = (time: Tone.Unit.Time) => {
+      onPlayNote(time)
+      beat = (beat + 1) % 8
+      setPLayingIndex(beat)
+    }
+
+    transportRef.current.bpm.value = 120
+    if (!isRepeatScheduled.current) {
+      transportRef.current.scheduleRepeat(onRepeat, '8n')
+      isRepeatScheduled.current = true
+    }
+  }, [destinationNode, gateNode])
+
   return (
-    <div className="grid grid-flow-row auto-rows-max w-full bg-white dark:bg-zinc-800">
-      {sequence.map((row) => (
-        <div key={row.id} className="grid grid-flow-col auto-cols-fr">
-          {row.value.map((item) => (
-            <button
-              key={item.id}
-              className={clsx('m-1 p-1 border rounded', {
-                'bg-green-500/75': item.isActive,
-              })}
-              onClick={() => handleClick(item.id)}
-            >
-              {item.note}
-            </button>
-          ))}
+    <div className="flex w-full space-x-2 bg-white dark:bg-zinc-800 p-2">
+      <div className="flex flex-col space-y-2">
+        <DestinationSelect
+          destinations={nodes}
+          initialValue={'not_set'}
+          onChange={handleDestinationChange}
+        />
+        <DestinationSelect
+          destinations={nodes}
+          initialValue={'not_set'}
+          onChange={handleGateChange}
+        />
+        <div>
+          <Button onClick={isPlaying ? stopSequence : playSequence}>
+            {isPlaying ? 'STOP' : 'START'}
+          </Button>
         </div>
-      ))}
+      </div>
+      <div className="grid grid-flow-row auto-rows-max w-full ">
+        {sequence.current.map((row) => (
+          <div key={row.id} className="grid grid-flow-col auto-cols-fr">
+            {row.value.map((item, index) => (
+              <button
+                key={item.id}
+                className={clsx('m-1 p-1 rounded border-2', {
+                  'bg-green-500/75': item.isActive,
+                  'border-green-500': index === playingIndex,
+                })}
+                onClick={() => handleClick(item)}
+              >
+                {item.note}
+              </button>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- src/utils.ts
+++ src/utils.ts
@@ -11,40 +11,21 @@ export const translateCoordinates = (
 })
 
 // https://medium.com/geekculture/creating-a-step-sequencer-with-tone-js-32ea3002aaf5
-// an array of notes is passed in as the argument to allow for flexbility.
-// const notes = ["F4", "Eb4", "C4", "Bb3", "Ab3", "F3"];
-
 export const makeGrid = () => {
   const notes = ['F4', 'Eb4', 'C4', 'Bb3', 'Ab3', 'F3']
-  // our "notation" will consist of an array with 6 sub arrays
-  // each sub array corresponds to one row of our sequencer
-
-  // declare the parent array to hold each row subarray
   const rows = []
 
   for (const note of notes) {
-    // declare the subarray
-    // const row = { id: crypto.randomUUID(), value: [] }
-    // each subarray contains multiple objects that have an assigned note
-    // and a boolean to flag whether they are active.
-    // each element in the subarray corresponds to one eighth note.
-    // for (let i = 0; i < 8; i++) {
-    //   row.value.push({
-    //     id: crypto.randomUUID(),
-    //     note: note,
-    //     isActive: false,
-    //   })
-    // }
     rows.push({
       id: crypto.randomUUID(),
-      value: Array.from({ length: 8 }, (_item) => ({
+      value: Array.from({ length: 8 }, () => ({
         id: crypto.randomUUID(),
         note: note,
         isActive: false,
+        isPlaying: false,
       })),
     })
   }
 
-  // we now have 6 rows each containing 8 eighth notes
   return rows
 }
