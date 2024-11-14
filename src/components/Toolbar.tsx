5d94a45c2f7892f57086886563eb70021d47bc39
Christopher Clemons <chris.clemons85@gmail.com>
2024-11-14 13:23:31 +0100

    update initial LFO frequency

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -84,7 +84,7 @@ export function ModuleListAdd({
             sources: [],
             destinations: [],
             settings: {
-              frequency: 10,
+              frequency: 5,
               type: 'sine',
             },
           })
